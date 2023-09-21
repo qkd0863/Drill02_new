5731a4c2b4379217ed97aea85554a269ac4c31a7
im-chaeeun <144077145+im-chaeeun@users.noreply.github.com>
2023-09-21 12:41:27 +0900

    간격수정

--- character_grass.py
+++ character_grass.py
@@ -16,22 +16,16 @@ def render_frame(x, y):
         clear_canvas_now()
         grass.draw_now(400,30)
         character.draw_now(x,y)
-        delay(0.01)          
-
-
+        delay(0.1)          
 
 def run_circle():
     print('circle')
     cx, cy , r = 400, 300, 200
     
-    for deg in range(270,630,5):
+    for deg in range(90,450+1,5):
         x = r * math.cos(math.radians(deg)) + cx
         y = r * math.sin(math.radians(deg)) + cy
         render_frame(x, y)
-
-
-    
-
 
 def run_rectangle():
     print('rectangle')
@@ -48,23 +42,10 @@ def run_rectangle():
         
     for y in range(550,90-1,-5):
         render_frame(50,y)
-
-
-
-
-        
-
-
     
-
-
-
-
-
-
 while(True):
     run_rectangle()
-    #run_circle()
+    run_circle()
     break
 
 
