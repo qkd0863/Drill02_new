c472c380f3446fdedaa1a55811f2ba58da44642e
im-chaeeun <144077145+im-chaeeun@users.noreply.github.com>
2023-09-21 12:20:03 +0900

    그리기 함수 추가

--- character_grass.py
+++ character_grass.py
@@ -11,6 +11,15 @@ grass.draw_now(400,30)
 character.draw_now(400,90)
 delay(1)
 
+
+def render_frame(x, y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)          
+
+
+
 def run_circle():
     print('circle')
     cx, cy , r = 400, 300, 200
@@ -18,10 +27,7 @@ def run_circle():
     for deg in range(270,630,5):
         x = r * math.cos(math.radians(deg)) + cx
         y = r * math.sin(math.radians(deg)) + cy
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)        
+        render_frame(x, y)
 
 
     
@@ -32,10 +38,8 @@ def run_rectangle():
 
     
     for x in range(50,750+1,5):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)       
+        render_frame(x, 90)
+
 
     
 
