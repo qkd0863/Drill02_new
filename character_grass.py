2ed64134342eef640f9309daa8ba1855e2c6b4f2
im-chaeeun <144077145+im-chaeeun@users.noreply.github.com>
2023-09-21 11:43:58 +0900

    draw canvas

--- character_grass.py
+++ character_grass.py
@@ -2,3 +2,14 @@ from pico2d import *
 
 open_canvas()
 
+
+character = load_image('character.png')
+grass = load_image('grass.png')
+
+clear_canvas_now()
+grass.draw_now(400,30)
+character.draw_now(400,90)
+delay(1)
+
+
+close_canvas()
